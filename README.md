b87d5c8b354d1790f1c94e50bb1e030d80756e4a
Nanoseb <sebastien.lemaire@soton.ac.uk>
2020-07-18 15:16:09 +0100

    add basic example

--- README.md
+++ README.md
@@ -1,17 +1,20 @@
 # PyMMS
 This python module generates Manufactured Solutions for RANS equations using Sympy.
 
-
 The following turbulence models have been implemented:
 - Spalart-Allmaras (1992)
 - One equation Eddy viscosity model from Menter (1997)
 
 ## Installation
+Can be installed and used like any python package, for example:
 ```
 pip3 install https://github.com/Nanoseb/PyMMS/archive/master.zip
 ```
 
 ## Usage
-see example folder.
+See examples folder:
+- `basic_example.py`: a very basic test case to show how PyMMS can be used
+- `3D_RANS_unsteady.py`: implementation of the Manufactured Solution defined in [Eca et al. (2012)](https://doi.org/10.1080/10618562.2012.717617)
+
 
 
---
+++ examples/basic_example.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+#
+# This file is a basic example to show how to use PyMMS
+# See 3D_RANS_unsteady.py for a more realistic and complex use case
+
+
+from sympy import *
+from PyMMS import PyMMS
+
+# Definition of basic symbols
+x, y, z, t = symbols('x y z t')
+
+########################################
+# Case initialisation
+########################################
+# Definition of parametric symbols
+T = symbols('Period')
+A = symbols('A')
+B = symbols('B')
+
+# Definition of parameters default values
+global_vars = [(T, 1),
+               (A, 10),
+               (B, 5)]
+
+Nu = 1
+rho = 1
+
+
+
+########################################
+# Definition of field functions
+########################################
+U = A*cos(2*pi*t/T)*cos(x)
+V = B*cos(2*pi*t/T)*cos(y)
+
+# W verifying the continuity equation
+W = - Integral(diff(U, x)+ diff(V, y), (z, 0, z))
+
+P = Integer(1)
+
+########################################
+# MMS generation and export
+########################################
+
+
+# Initialisation of PyMMS with field quantities
+mms = PyMMS(Nu=Nu,
+            rho=rho,
+            U=U,
+            V=V,
+            W=W,
+            P=P,
+            turbulence_model="none")
+
+# Computation of source terms
+mms.compute_sources()
+
+# Export of a Fortran module file
+mms.export_module("module-basic-example.F90",
+                  global_vars=global_vars)
+
+
